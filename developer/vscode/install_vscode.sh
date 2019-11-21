93da7ab2dfe6a208d574869b5212e5977bcea333
Lung-yu (Tygr) <workfile975@gmail.com>
2019-11-21 19:51:12 +0800

    upgrade script on Ubuntu 18.04

--- developer/vscode/install_vscode.sh
+++ developer/vscode/install_vscode.sh
@@ -2,3 +2,6 @@ wget https://vscode.cdn.azure.cn/stable/27492b6bf3acb0775d82d2f87b25a93490673c6d
 sudo dpkg -i code_1.16.1-1505406497_amd64.deb
 rm code_1.16.1-1505406497_amd64.deb
 
+
+sudo apt update
+sudo apt install code
